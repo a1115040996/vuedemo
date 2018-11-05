18e72cf00434dfcc0969c01142f99bde0a19dca8
飞尽堂前燕 <1115040996@qq.com>
2018-11-05 15:50:49 +0800

    父子通信，父子传参实例

---
+++ src/components/index/components/child.vue
@@ -0,0 +1,35 @@
+<template>
+  <div @click="handleClick">
+    {{childMessage.msg.name}}
+    <div @click="handleSendMsg">点击子类执行父类方法</div>
+  </div>
+</template>
+<script>
+    export default {
+        name: "child",
+        data:function(){
+          return{
+            name:'我是子组件'
+          }
+        },
+        methods:{
+          handleClick:function(){
+            console.log(this.childFunctin);
+          },
+          handleSendMsg:function(){
+            this.childFunctin.sayHi({
+              name:'子集传递的数据',
+              content:'hello i am max'
+            })
+          }
+        },
+      props:{
+        childMessage:Object,
+        childFunctin:Object
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    {{name}}
+    <chi :childMessage="childMessage" :childFunctin="childFunctin"></chi>
+
+    <div id="clickme" @click="sendMessageForChild">点击我 向子组件传递数据</div>
+
+    <div >{{childMessage.handle.name}} ---  {{childMessage.handle.content}}</div>
+  </div>
+</template>
+
+<script>
+  import chi from '@/components/index/components/child';
+    export default {
+        name: "index",
+        data:function(){
+          return {
+            name:'hello world',
+            index:0,
+            childMessage:{
+              msg:{
+                name:'zhangsan',
+                age:18
+              },
+              handle:{
+                name:'',
+                content:''
+              }
+            },
+            childFunctin:{
+              sayHi:function(msg){
+                this.childMessage.handle = msg;
+              }.bind(this)
+            }
+          }
+        },
+        methods:{
+          sendMessageForChild:function(){
+            this.index+=1;
+            console.log('点击执行')
+            this.childMessage.msg.name = '下标值相累加'+this.index;
+          }
+        },
+      components: { chi : chi },
+    }
+</script>
+
+<style scoped>
+
+</style>
